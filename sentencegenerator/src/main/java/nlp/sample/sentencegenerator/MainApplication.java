afb688253c99928790032cadf3149e1987111a84
meihuno <meihuno.huno.san@gmail.com>
2016-09-24 18:34:37 +0900

    the first commit

---
+++ sentencegenerator/src/main/java/nlp/sample/sentencegenerator/MainApplication.java
@@ -0,0 +1,19 @@
+package nlp.sample.sentencegenerator;
+
+import nlp.sample.sentencegenerator.SentenceGenerator;
+
+public class MainApplication {
+
+	public static void main(String[] args) throws Exception {		
+		/*
+		String dirname = "/Users/sugihara/void/data/aozora_bunko/dazai_osamu";
+		String smoothingType = "backoff";
+		SentenceGenerator sg = new SentenceGenerator();
+		sg.createNGramModel(dirname, smoothingType);
+		System.out.println(sg.retRandomSentence(100, true, true));
+		System.out.println(sg.retRandomSentence(100, true, false));
+		System.out.println(sg.retRandomSentence(100, false, false));
+		*/
+	}
+
+}
