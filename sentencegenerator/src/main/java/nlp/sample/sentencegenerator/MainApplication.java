c3d1de1b996a733649fff28320b9617821856575
meihuno <meihuno.huno.san@gmail.com>
2016-09-30 00:12:39 +0900

    after making mvn_repo

--- sentencegenerator/src/main/java/nlp/sample/sentencegenerator/MainApplication.java
+++ sentencegenerator/src/main/java/nlp/sample/sentencegenerator/MainApplication.java
@@ -5,11 +5,15 @@ import nlp.sample.sentencegenerator.SentenceGenerator;
 public class MainApplication {
 
 	public static void main(String[] args) throws Exception {		
+		
 		/*
 		String dirname = "/Users/sugihara/void/data/aozora_bunko/dazai_osamu";
 		String smoothingType = "backoff";
 		SentenceGenerator sg = new SentenceGenerator();
 		sg.createNGramModel(dirname, smoothingType);
+		*/
+		System.out.println("This is a test line.");
+		/*
 		System.out.println(sg.retRandomSentence(100, true, true));
 		System.out.println(sg.retRandomSentence(100, true, false));
 		System.out.println(sg.retRandomSentence(100, false, false));
